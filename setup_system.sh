668c6046ffce1bdac6188febf548089bf4d26c3f
Jeremy Schaub <schaubjeremy@gmail.com>
2015-12-18 10:27:16 -0600

    Fixed .vimrc and set NOPASSWD for ubuntu user

--- setup_system.sh
+++ setup_system.sh
@@ -5,7 +5,7 @@ echo '"\e[B": history-search-forward' >> ~/.inputrc
 cp ~/.inputrc /home/ubuntu/.
 chown ubuntu:ubuntu /home/ubuntu/.inputrc
 
-echo "syntax enable" >> ~/.vimrc
+echo "syntax enable" > ~/.vimrc
 echo "set background=dark" >> ~/.vimrc
 echo "set tabstop=4 shiftwidth=4 expandtab" >> ~/.vimrc
 echo "set ruler" >> ~/.vimrc
@@ -14,6 +14,13 @@ echo "set relativenumber" >> ~/.vimrc
 echo "inoremap {<cr> {<cr>}<c-o>O<tab>" >> ~/.vimrc
 echo "inoremap [<cr> [<cr>]<c-o>O<tab>" >> ~/.vimrc
 echo "inoremap (<cr> (<cr>)<c-o>O<tab>" >> ~/.vimrc
+
+FLAG=$(grep ubuntu /etc/sudoers | grep NOPASSWD | wc -l)
+if [ $FLAG -eq 0 ]
+then
+  echo Setting NOPASSWD option for user ubuntu
+  echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+fi
 
 cp ~/.vimrc /home/ubuntu/.
 chown ubuntu:ubuntu /home/ubuntu/.vimrc
